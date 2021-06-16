a26292a0aff5e18ba5a4a3da9aa8818375db088e
unitythemaker <unitythemaker+github@gmail.com>
2021-06-16 04:00:27 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { Client as DClient } from 'discord.js';
+const client = new DClient();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(process.env["TOKEN"]);
